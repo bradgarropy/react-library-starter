92ff493cc05e6c0a9f1f1972a0a80a6b06924f6c
Brad Garropy <bradgarropy@gmail.com>
2021-01-26 23:00:06 -0600

    update test.

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,8 +1,9 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
+
 import {Thing} from "../src"
 
-describe("it", () => {
+describe("it does something", () => {
     it("renders without crashing", () => {
         const div = document.createElement("div")
         ReactDOM.render(<Thing />, div)
