351470b8f2b443a572551e537f4565d305806a0a
Brad Garropy <bradgarropy@gmail.com>
2021-03-07 21:32:43 -0600

    add github actions. add precommit lint and format. remove tsdx.

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ const config = {
     clearMocks: true,
     collectCoverage: true,
     collectCoverageFrom: ["<rootDir>/src/**"],
+    transform: {
+        "^.+\\.(ts|tsx)$": "ts-jest",
+    },
     verbose: true,
 }
 
